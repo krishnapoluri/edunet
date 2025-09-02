8a037d04e0cdf0015aed155d5b3e1146d88b18a1
Adhwaith Krishna <188646135+krishnapoluri@users.noreply.github.com>
2025-09-02 23:23:30 +0530

    frontend.py

---
+++ frontend.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        # Create a dummy node to build the merged list
+        dummy = ListNode()
+        tail = dummy
+
+        # Traverse both lists while both are non-empty
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        # Attach whichever list is still non-empty
+        tail.next = list1 if list1 else list2
+
+        return dummy.next
